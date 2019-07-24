de4f30ae2c119dee891fc34a1867f40788eef36d
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-07-24 01:05:45 -0400

    dom manipulation code added

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("Script Running!");
+
+const btn = document.querySelector("#btn");
+const title = document.querySelector("h1");
+
+btn.addEventListener('click', (e) => {
+  title.innerHTML = "Congradulations, You Won $1000 !!!";
+  title.style.color = "blue";
+})
+
+btn.addEventListener('mouseover', (e) => {
+  btn.style.background = "black";
+})
+
+btn.addEventListener('mouseleave', (e) => {
+  btn.style.background = "green";
+})
